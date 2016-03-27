7661f77d9e69c337e1932845187970e1a0cc75f1
David Richfield <davidrichfield@gmail.com>
2016-03-27 23:17:45 +0200

    Skip record on key error; commit outside loop
    Use try/except clause to skip records without descriptions.
    Don't commit every record separately; that's awful for performance.

--- database/jsontodb.py
+++ database/jsontodb.py
@@ -14,7 +14,12 @@ clubdata=json.load(infile)
 con = sqlite3.connect('test.db')
 
 #At the moment, the geojson just has a single text field
+#FIXME: Inserted up to <A HREF="http://www.go-lingen.de">Lingen</A> and then failed with "KeyError: 'description'"
 for club in clubdata['features']:
-  con.execute("INSERT INTO clubs (layer, lon, lat, name) VALUES ('Deutschland', ?, ?, ?)", (club['geometry']['coordinates'][0], club['geometry']['coordinates'][1], club['properties']['description']))
-  #Write it to the database.
-  con.commit()
+  try:
+    con.execute("INSERT INTO clubs (layer, lon, lat, name) VALUES ('Deutschland', ?, ?, ?)", (club['geometry']['coordinates'][0], club['geometry']['coordinates'][1], club['properties']['description']))
+    #Write it to the database.
+  except KeyError:
+    pass
+con.commit()
+
