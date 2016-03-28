9331f94da8778c1c78a376997d9e04ff79a70ac8
David Richfield <davidrichfield@gmail.com>
2016-03-28 06:25:47 +0200

    Add club type and status tables.

--- database/makedb.py
+++ database/makedb.py
@@ -13,6 +13,24 @@ CREATE TABLE layers
 )
 ''')
 con.execute('''
+CREATE TABLE clubtypes
+-- List of known club types with marker types
+(
+  name TEXT PRIMARY KEY, --What we call the type (e.g. "club" or "individual")
+  iconUrl TEXT, --What icon picture to use for this type of club
+  description TEXT --What does this type include
+)
+''')
+con.execute('''
+CREATE TABLE clubstatuses
+-- List of known club statuses with marker types
+(
+  name TEXT PRIMARY KEY, --What we call the status (e.g. "active")
+  iconClass TEXT, --What marker shape to use for this status
+  description TEXT --What does this status mean?
+)
+''')
+con.execute('''
 CREATE TABLE clubs
 -- A list of all the go clubs in the world
 (
@@ -25,6 +43,10 @@ CREATE TABLE clubs
   meetplace TEXT, --Meeting place of the club (as text address)
   meettime TEXT, --Meeting times of the club (as text for now)
   contact TEXT, --Contact details (Chuck all into one text field for now)
+  clubtype TEXT, --Type of club: is it a single player or a fixed club? foreign key
+  clubstatus TEXT, --Club status: active or inactive? foreign key
+  FOREIGN KEY(clubstatus) REFERENCES clubstatuses(name), --Type of club: is it a single player or a fixed club?
+  FOREIGN KEY(clubtype) REFERENCES clubtypes(name), --Type of club: is it a single player or a fixed club?
   FOREIGN KEY(layer) REFERENCES layers(name) --Which layer is it on?
 )
 ''')
@@ -34,4 +56,8 @@ con.execute("INSERT INTO layers (name, description, color) VALUES ('FR',?,?)", (
 con.execute("INSERT INTO layers (name, description, color) VALUES ('NL',?,?)", (u'Nederlandse Go-Bond: https://gobond.nl/verenigingen', 'Orange'))
 con.execute("INSERT INTO layers (name, description, color) VALUES ('UK',?,?)", (u'British Go Association: http://www.britgo.org/clubs/list', 'Red'))
 con.execute("INSERT INTO layers (name, description, color) VALUES ('ZA',?,?)", (u'South African Go Association: http://www.sagoclubs.co.za/', 'DarkGreen'))
+con.execute("INSERT INTO clubstatuses (name, description) VALUES ('active','Currently active club')")
+con.execute("INSERT INTO clubstatuses (name, iconClass, description) VALUES ('inactive','Ball','Inactive club')")
+con.execute("INSERT INTO clubtypes (name, iconUrl, description) VALUES ('individual','/uploads/pictogram/pitch-24.png','Individual offering games')")
+con.execute("INSERT INTO clubtypes (name, description) VALUES ('club','Regular club meeting')")
 con.commit()
