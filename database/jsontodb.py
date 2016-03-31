2da6be987f2979df3810c01a206b6964873756b9
David Richfield <davidrichfield@gmail.com>
2016-03-31 21:32:45 +0200

    Get db name from command-line; read club name
    Get the database name from the command-line as a parameter.
    Use the "name" field from the GeoJSON file as the club name.

--- database/jsontodb.py
+++ database/jsontodb.py
@@ -11,13 +11,12 @@ infile=open(sys.argv[1],'r')
 clubdata=json.load(infile)
 
 #Open database connection
-con = sqlite3.connect('test.db')
+con = sqlite3.connect(sys.argv[2])
 
-#At the moment, the geojson just has a single text field, instead of name and description in most cases.
+#At the moment, the geojson just has a name and description, with all the information bundled into the description
 for club in clubdata['features']:
   try:
-    con.execute("INSERT INTO clubs (layer, lon, lat, name) VALUES (?, ?, ?, ?)", (club['layer'], club['geometry']['coordinates'][0], club['geometry']['coordinates'][1], club['properties']['description']))
-    #Write it to the database.
+    con.execute("INSERT INTO clubs (layer, lon, lat, name, contact) VALUES (?, ?, ?, ?, ?)", ('RU', club['geometry']['coordinates'][0], club['geometry']['coordinates'][1], club['properties']['name'], club['properties']['description'])) #FIXME: currently hard-coded layer info.
   except KeyError:
     pass
 con.commit()
