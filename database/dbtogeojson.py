a884b19dc08889f09dfd7f2b11924ebd4d315ce9
David Richfield <davidrichfield@gmail.com>
2016-04-02 23:43:15 +0200

    Iterate over layers, creting file for each.
    Create a geojson dump for each layer in the database; good for upload to
    umap.

--- database/dbtogeojson.py
+++ database/dbtogeojson.py
@@ -1,26 +1,22 @@
-import sqlite3, os, urllib, json
+#!/usr/bin/python
+import sqlite3, os, urllib, json, codecs
 
 conn = sqlite3.connect('clubs.db')
 c = conn.cursor()
-#FIXME: Need to add handling for club type and status, and not hardcode layer
-c.execute('''
-SELECT
-  name,
-  website,
-  meetplace,
-  meettime,
-  contact,
-  lat,
-  lon,
-  layer
-FROM 
-  clubs 
-WHERE layer LIKE "CH"
-''')
+#FIXME: Need to add handling for club type and status
+c.execute("SELECT name FROM layers;")
 result = c.fetchall()
-c.close()
-features=[]
-for row in result:
-    features.append({'geometry': {'type': 'Point', 'coordinates': [row[6],row[5]]}, 'layer': row[7], 'type': 'Feature', 'properties': {'description': ', '.join(filter(None,row[1:5])), 'name': row[0]}})
-result={'type': 'FeatureCollection', 'features': features}
-print(json.dumps(result))
+for layer in result:
+  outfile=codecs.open(layer[0]+"_go_clubs.json",'w', encoding="utf-8")
+  c.execute('''
+  SELECT name, website, meetplace, meettime, contact, lat, lon
+  FROM 
+    clubs 
+  WHERE layer = ?
+  ''', (layer[0],))
+  result = c.fetchall()
+  features=[]
+  for row in result:
+      features.append({'geometry': {'type': 'Point', 'coordinates': [row[6],row[5]]}, 'layer': layer[0], 'type': 'Feature', 'properties': {'description': ', '.join(filter(None,row[1:5])), 'name': row[0]}})
+  result={'type': 'FeatureCollection', 'features': features}
+  json.dump(result, outfile, indent=2, ensure_ascii=False)
