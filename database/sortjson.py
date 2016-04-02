27f8ed84c57bc2eff03b104d320ac41dfd03c49b
David Richfield <davidrichfield@gmail.com>
2016-04-02 20:42:39 +0200

    better unicode handling

--- database/sortjson.py
+++ database/sortjson.py
@@ -2,14 +2,13 @@
 
 #Script to sort umap geojson data for clearer diff
 
-import json, sys
+import json, sys, codecs
 
 #Process each file given as an argument on the command line
 for i in sys.argv[1:]:
   infile=open(i,'r')
-  outfile=open("sorted."+i,'w')
+  outfile=codecs.open("sorted."+i,'w', encoding="utf-8")
   #Parse it as json data
-  dicti=json.load(infile)
+  dicti=json.loads(infile.read().decode("utf-8-sig"))
   #Write sorted data to outfile
-  for j in sorted(dicti['features']):
-    outfile.write(str(j)+'\n')
+  json.dump(dicti, outfile, indent=2, sort_keys=True, ensure_ascii=False)
