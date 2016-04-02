aadb2c53ebe7ae54eef09e4d5ee1befd2dc70d51
David Richfield <davidrichfield@gmail.com>
2016-04-02 15:09:41 +0200

    start writing a script to sort umap geojson data

---
+++ database/sortjson.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+#Script to sort umap geojson data for clearer diff
+
+import json, sys
+
+#Process each file given as an argument on the command line
+for i in sys.argv[1:]:
+  infile=open(i,'r')
+  outfile=open("sorted."+i,'w')
+  #Parse it as json data
+  dicti=json.load(infile)
+  #Write sorted data to outfile
+  outfile.write(sorted(dicti['features']))
