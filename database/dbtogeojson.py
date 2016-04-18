e542ec6e0bd2371c21a539898b8a340975e0bdc4
David Richfield <davidrichfield@gmail.com>
2016-04-18 23:13:55 +0200

    Add code to render individual and defunct clubs
    Add code to render individual players as map pins and defunct clubs as
    dots.

--- database/dbtogeojson.py
+++ database/dbtogeojson.py
@@ -12,10 +12,15 @@ c.execute("SELECT name, id FROM goclubdb_layer;")
 result = c.fetchall()
 for layer in result:
   outfile=codecs.open(layer[0]+"_go_clubs.json",'w', encoding="utf-8")
-  c.execute("SELECT name, website, meetplace, meettime, contact, lat, lon FROM goclubdb_club WHERE layer_id = %s;", (layer[1],))
+  c.execute("SELECT name, website, meetplace, meettime, contact, lat, lon, clubtype_id, clubstatus_id FROM goclubdb_club WHERE layer_id = %s;", (layer[1],))
   result = c.fetchall()
   features=[]
   for row in result:
-      features.append({'geometry': {'type': 'Point', 'coordinates': [row[6],row[5]]}, 'layer': layer[0], 'type': 'Feature', 'properties': {'description': ' , '.join(filter(None,row[1:5])), 'name': row[0]}})
+      if row[7]==7:
+          features.append({'geometry': {'type': 'Point', 'coordinates': [row[6],row[5]]}, 'layer': layer[0], 'type': 'Feature', 'properties': {'description': ' , '.join(filter(None,row[1:5])), 'name': row[0]}, '_storage_options': {'iconClass': 'Ball'}} )
+      elif row[8]==2:
+          features.append({'geometry': {'type': 'Point', 'coordinates': [row[6],row[5]]}, 'layer': layer[0], 'type': 'Feature', 'properties': {'description': ' , '.join(filter(None,row[1:5])), 'name': row[0]}, '_storage_options': {'iconClass': 'Circle'}} )
+      else:
+          features.append({'geometry': {'type': 'Point', 'coordinates': [row[6],row[5]]}, 'layer': layer[0], 'type': 'Feature', 'properties': {'description': ' , '.join(filter(None,row[1:5])), 'name': row[0]}})
   result={'type': 'FeatureCollection', 'features': features}
   json.dump(result, outfile, indent=2, ensure_ascii=False, encoding='utf8')
