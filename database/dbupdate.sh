5a7b2d4bed3ebaf3ae012ecc57596b2f38e7d055
David Richfield <davidrichfield@gmail.com>
2016-04-30 22:19:58 +0200

    Download JSON from server instead of database

--- database/dbupdate.sh
+++ database/dbupdate.sh
@@ -1,4 +1,3 @@
-python dbtogeojson.py
 for i in `ls -d [A-Z][A-Z]`
-  do mv `printf "$i%s" "_go_clubs.json"` $i
+  do curl -H "Accept: application/json" https://goclubdb.herokuapp.com/clubs/$i/json > `printf "$i/$i%s" "_go_clubs.json"`
 done
